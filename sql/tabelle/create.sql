54176fac4bccd035a7b0d62f387dd85e740bddee
Giulio Fantuzzi <74678041+giuliofantuzzi@users.noreply.github.com>
2023-07-06 09:06:47 +0200

    Update create.sql

--- sql/tabelle/create.sql
+++ sql/tabelle/create.sql
@@ -1,11 +1,20 @@
+/*-------------------------------------------*/
 -- DB SETUP 
 /*-------------------------------------------*/
+
 DROP DATABASE IF EXISTS PrimaCategoriaVeneta;
 CREATE DATABASE PrimaCategoriaVeneta;
 USE PrimaCategoriaVeneta;
+
+/*-------------------------------------------*/
+-- CREAZIONE DELLE TABELLE
 /*-------------------------------------------*/
 
--- TABLE CREATION
+DROP TABLE IF EXISTS Girone;
+CREATE TABLE Girone(
+	Lettera char(1) PRIMARY KEY,
+    CONSTRAINT GironeValido CHECK (Lettera BETWEEN 'A' AND 'H')
+);
 
 DROP TABLE IF EXISTS Allenatore;
 CREATE TABLE Allenatore(
@@ -16,17 +25,12 @@ CREATE TABLE Allenatore(
     Patentino bool DEFAULT FALSE
 );
 
-DROP TABLE IF EXISTS Girone;
-CREATE TABLE Girone(
-	Lettera char(1) PRIMARY KEY
-);
-
 DROP TABLE IF EXISTS Stadio;
 CREATE TABLE Stadio(
 	Nome varchar(40) PRIMARY KEY,
     Indirizzo varchar(40),
-    Capienza int(4) NOT NULL,
-    CostoBiglietto int(2) NOT NULL
+    Capienza int(4) NOT NULL CHECK(Capienza>0),
+    CostoBiglietto int(2) NOT NULL CHECK(CostoBiglietto>0)
 );
 
 DROP TABLE IF EXISTS Arbitro;
@@ -35,7 +39,7 @@ CREATE TABLE Arbitro(
     Nome varchar(25),
     Cognome varchar(25),
     DataDiNascita date,
-    PaeseDiResidenza varchar(30) NOT NULL,
+    PaeseDiResidenza varchar(30) NOT NULL, -- mi servirà per controllo su assegnazione arbitro
     Sezione varchar(30) NOT NULL
 );
 
@@ -47,8 +51,8 @@ CREATE TABLE Squadra(
     Vittorie int(2) DEFAULT 0,
     Pareggi int(2) DEFAULT 0,
     Sconfitte int(2) DEFAULT 0,
-    GoalFatti int(3) DEFAULT 0,
-    GoalSubiti int(3) DEFAULT 0,
+    GoalFatti int(3) DEFAULT 0 CHECK(GoalFatti>=0),
+    GoalSubiti int(3) DEFAULT 0 CHECK(GoalSubiti>=0),
     Allenatore char(16) UNIQUE, -- grazie a unique non potrò avere squadre con lo stesso allenatore
     Stadio varchar(40) NOT NULL UNIQUE, -- squadra deve per forza avere stadio e non ci sono squadre con lo stesso
     FOREIGN KEY(Girone) REFERENCES Girone(Lettera),
@@ -73,12 +77,12 @@ DROP TABLE IF EXISTS Partita;
 CREATE TABLE Partita(
 	ID char(5) PRIMARY KEY,
     Data date NOT NULL,
-    Giornata int(2) NOT NULL,
-    Spettatori int(4) DEFAULT 0,
+    Giornata int(2) NOT NULL CHECK(Giornata>=1),
+    Spettatori int(4) DEFAULT 0 CHECK(Spettatori>=0),
     SquadraCasa varchar(25) NOT NULL,
     SquadraTrasferta varchar(25) NOT NULL,
-    GoalCasa int(2) DEFAULT NULL,
-    GoalTrasferta int(2) DEFAULT NULL,
+    GoalCasa int(2) DEFAULT NULL CHECK(GoalCasa>=0),
+    GoalTrasferta int(2) DEFAULT NULL CHECK(GoalTrasferta>=0),
     Stadio varchar(40) NOT NULL,
     Arbitro char(16), -- non metto NOT NULL, così la designazione arbitrale può avvenire dopo l'inserimento della partita
     FOREIGN KEY(SquadraCasa) REFERENCES Squadra(Nome),
@@ -92,11 +96,10 @@ CREATE TABLE Partecipazione(
 	Giocatore char(16),
     Partita char(5),
     Ruolo varchar(20) CHECK(Ruolo IN ('Portiere', 'Difensore','Centrocampista','Attaccante')),
-    Goal int(2) DEFAULT 0,
+    Goal int(2) DEFAULT 0 CHECK(Goal>=0),
     Ammonizione BOOL DEFAULT FALSE,
     Espulsione BOOL DEFAULT FALSE,
     PRIMARY KEY (Giocatore,Partita),
     FOREIGN KEY(Giocatore) REFERENCES Giocatore(CF),
     FOREIGN KEY(Partita) REFERENCES Partita(ID)
 );
-
