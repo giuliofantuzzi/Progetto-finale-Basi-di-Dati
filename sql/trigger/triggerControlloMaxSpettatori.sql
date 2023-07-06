1d2325df50fa784750e39e5fb29a72816781221a
Giulio Fantuzzi <74678041+giuliofantuzzi@users.noreply.github.com>
2023-07-06 10:51:42 +0200

    Update triggerControlloMaxSpettatori.sql

--- sql/trigger/triggerControlloMaxSpettatori.sql
+++ sql/trigger/triggerControlloMaxSpettatori.sql
@@ -4,13 +4,13 @@ CREATE TRIGGER ControlloMaxSpettatori
 BEFORE UPDATE ON Partita
 FOR EACH ROW
 BEGIN
-    DECLARE stadioCapienza INT;
+    DECLARE capienzastadio INT;
     
-    SELECT Capienza INTO stadioCapienza
+    SELECT Capienza INTO capienzastadio
     FROM Stadio
     WHERE Nome = OLD.Stadio; -- stadio viene già inserito in inserimento partita!
     
-    IF NEW.Spettatori > stadioCapienza THEN
+    IF NEW.Spettatori > capienzastadio THEN
         SIGNAL SQLSTATE '45004'
         SET MESSAGE_TEXT = 'Il numero di spettatori supera la capienza dello stadio. Inserimento non consentito.';
     END IF;
